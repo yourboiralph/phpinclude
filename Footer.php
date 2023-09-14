36a518835e91ea1fa6e98514a99e4218ad037fd6
Ralph Hernandez <ralph@Ralphs-MacBook-Air.local>
2023-09-15 00:03:48 +0800

    updated

--- Footer.php
+++ Footer.php
@@ -1,5 +1,14 @@
 <footer class="">
     <div class="flex justify-center bg-primary">
-        <h4>&copy; Copyright 2023</h4>
+        <h4>&copy; Copyright <span id="todayYear"></span></h4>
     </div>
 </footer>
+
+<script>
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const todayYear = document.getElementById("todayYear");
+
+todayYear.innerText = currentYear;
+
+</script>
